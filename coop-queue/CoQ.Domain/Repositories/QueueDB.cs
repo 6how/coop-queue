05f7359f9d739250892f6ebf63baef0672e3f67c
glasercr <glasercr@mail.uc.edu>
2019-10-29 10:17:16 -0400

    Added Domain and Models projects to the solution. Renamed the web project to CoQ.Web for naming consistency.

---
+++ coop-queue/CoQ.Domain/Repositories/QueueDB.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CoQ.Domain.Repositories
+{
+    //This repository is going to be used to call from our internal DB.
+    public class Class1
+    {
+    }
+}
---
+++ coop-queue/CoQ.Domain/Repositories/SteamDB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoQ.Domain.Repositories
+{
+    //This repository is going to be used strictly for Steam API calls.
+    class SteamDB
+    {
+    }
+}
