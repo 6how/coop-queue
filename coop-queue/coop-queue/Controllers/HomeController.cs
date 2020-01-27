80ea61753df2b95b99fe5445f9ef0a3008b65305
Bri Liston <briliston@gmail.com>
2020-01-27 12:48:05 -0500

    Working on message section

--- coop-queue/coop-queue/Controllers/HomeController.cs
+++ coop-queue/coop-queue/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ namespace coop_queue.Controllers
             return View();
         }
 
+        public IActionResult MessageThread()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
