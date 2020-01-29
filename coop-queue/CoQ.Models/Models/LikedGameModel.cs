038cd855ff94dfd5f249add197b1e888a203f4cc
Cole <glasercr@mail.uc.edu>
2020-01-28 19:46:37 -0500

    Added some models and set up viewmodel for profile page.

---
+++ coop-queue/CoQ.Models/Models/LikedGameModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoQ.Models.Models
+{
+    public class LikedGameModel
+    {
+        public int LikedGameID { get; set; }
+
+        public int UserID { get; set; }
+
+        public GameModel Game { get; set; }
+
+        public DateTime LikedOnDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
--- coop-queue/coop-queue/Controllers/HomeController.cs
+++ coop-queue/coop-queue/Controllers/HomeController.cs
@@ -5,42 +5,59 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using coop_queue.Models;
+using CoQ.Web.Models.ViewModels;
+using CoQ.Models.Models;
 
 namespace coop_queue.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public ActionResult Privacy()
         {
             return View();
         }
 
-        public IActionResult Profile()
+        public ActionResult Profile()
+        {
+            List<FriendshipModel> testFriends = new List<FriendshipModel>();
+            testFriends.Add(coleAbdou);
+            testFriends.Add(coleTest);
+
+            List<LikedGameModel> testLikedGames = new List<LikedGameModel>();
+            testLikedGames.Add(coleHalo);
+            testLikedGames.Add(coleLast);
+
+            ProfileViewModel viewModel = new ProfileViewModel
+            {
+                User = coleGlaser,
+                Friends = testFriends,
+                LikedGames = testLikedGames
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Friends()
         {
             return View();
         }
 
-        public IActionResult Friends()
+        public ActionResult Likes()
         {
             return View();
         }
 
-        public IActionResult Likes()
+        public ActionResult Messages()
         {
             return View();
         }
 
-        public IActionResult Messages()
-        {
-            return View();
-        }
-
-        public IActionResult MessageThread()
+        public ActionResult MessageThread()
         {
             return View();
         }
@@ -50,5 +67,105 @@ namespace coop_queue.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+        #region TestData
+        public UserModel testUser1 = new UserModel
+        {
+            Email = "testUser1@gmail.com",
+            UserDescription = "I am a test user. The first of my line. Come play games with me!",
+            UserID = 1,
+            UserName = "Test User",
+            IsActive = true
+        };
+
+        public UserModel coleGlaser = new UserModel
+        {
+            Email = "glasercr@mail.uc.edu",
+            UserDescription = "I am me. Nothing really else to say.",
+            UserID = 2,
+            UserName = "Cole Glaser",
+            IsActive = true
+        };
+
+        public UserModel abdouFall = new UserModel
+        {
+            Email = "fallal@mail.uc.edu",
+            UserDescription = "My name is Abdou and I am the homie!",
+            UserID = 3,
+            UserName = "Abdou Fall",
+            IsActive = true
+        };
+
+        public FriendshipModel coleAbdou = new FriendshipModel
+        {
+            FriendAddedOn = DateTime.Now.AddDays(-4),
+            FriendFromID = 2,
+            FriendToID = 3,
+            FriendshipID = 1,
+            IsActive = true
+        };
+
+        public FriendshipModel coleTest = new FriendshipModel
+        {
+            FriendAddedOn = DateTime.Now.AddDays(-7),
+            FriendFromID = 1,
+            FriendToID = 2,
+            FriendshipID = 2,
+            IsActive = true
+        };
+
+        public GameModel haloMCC = new GameModel
+        {
+            GameID = 1,
+            GameName = "Halo: The Master Chief Collection",
+            GameScore = 8,
+            GameSystem = "PC",
+            IsActive = true
+        };
+
+        public GameModel lastOfUS = new GameModel
+        {
+            GameID = 2,
+            GameName = "The Last Of Us",
+            GameScore = 9.75,
+            GameSystem = "Playstation",
+            IsActive = true
+        };
+
+        public LikedGameModel coleHalo = new LikedGameModel
+        {
+            LikedGameID = 1,
+            UserID = 2,
+            LikedOnDate = DateTime.Now.AddDays(-16),
+            Game = new GameModel
+            {
+                GameID = 1,
+                GameName = "Halo: The Master Chief Collection",
+                GameScore = 8,
+                GameSystem = "PC",
+                IsActive = true
+            },
+            IsActive = true
+        };
+
+        public LikedGameModel coleLast = new LikedGameModel
+        {
+            LikedGameID = 2,
+            UserID = 2,
+            LikedOnDate = DateTime.Now.AddDays(-700),
+            Game = new GameModel
+            {
+                GameID = 2,
+                GameName = "The Last Of Us",
+                GameScore = 9.75,
+                GameSystem = "Playstation",
+                IsActive = true
+            },
+            IsActive = true
+        };
+
+        #endregion
     }
 }
